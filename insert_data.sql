17860df2f144fdcc781a3d32682ea461724d251d
Helton Junior <159181990+HeltonLCJunior@users.noreply.github.com>
2026-05-29 21:28:13 -0300

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO PESSOAS (Nome, Idade, Sexo, Cpf, Cidade, Estado, Profissao) VALUES 
+('Luciana Rayssa Novaes', 41, 'F', '571.821.421-20', 'Brasília', 'DF', 'Engenheira'),
+('Luciana Mariane Pires', 35, 'F', '918.871.371-73', 'Brasília', 'DF', 'Professora'),
+('Andrea Maria Silveira', 42, 'F', '076.204.031-91', 'Brasília', 'DF', 'Médica'),
+('Rebeca Tereza Barros', 27, 'F', '139.612.631-75', 'Brasília', 'DF', 'Advogada'),
+('Davi Isaac Vitor Almeida', 50, 'M', '311.514.531-45', 'Brasília', 'DF', 'Arquiteto'),
+('Kamilly Sarah Vieira', 31, 'F', '118.698.271-39', 'Brasília', 'DF', 'Designer'),
+('Joaquim Caio Arthur Monteiro', 22, 'M', '261.829.731-88', 'Brasília', 'DF', 'Estudante'),
+('Heloísa Alves Almeida', 45, 'F', '831.328.788-93', 'Brasília', 'DF', 'Enfermeira'),
+('Rosângela Vanessa Luna Ferreira', 38, 'F', '453.609.491-12', 'Brasília', 'DF', 'Programadora'),
+('Enrico Martin Caleb da Mota', 26, 'M', '432.816.311-69', 'Brasília', 'DF', 'Jornalista'),
+('Giovanni Samuel Drumond', 33, 'M', '987.321.831-94', 'Brasília', 'DF', 'Contador'),
+('Carlos Vinicius Carvalho', 38, 'M', '938.453.578-53', 'Brasília', 'DF', 'Motorista');
